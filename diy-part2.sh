3041731c8099571c9bbb01cb194a7f56ba28471b
YeeJong <yeezwo@gmail.com>
2026-08-10 00:18:38 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -18,6 +18,11 @@
 
 # Modify hostname
 #sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
-# 强行注入 Q3000 的 DTS 图纸和配置文件
-cp -f $GITHUB_WORKSPACE/mt7981b-ikuai-q3000.dts target/linux/mediatek/dts/mt7981b-ikuai-q3000.dts
+# 1. 在 OpenWrt 源码里建一个 mediatek 的专属包厢
+mkdir -p target/linux/mediatek/dts/mediatek
+
+# 2. 把图纸精准复制到包厢里面！
+cp -f $GITHUB_WORKSPACE/mt7981b-ikuai-q3000.dts target/linux/mediatek/dts/mediatek/mt7981b-ikuai-q3000.dts
+
+# 3. 正常替换配置文件
 cp -f $GITHUB_WORKSPACE/filogic.mk target/linux/mediatek/image/filogic.mk
